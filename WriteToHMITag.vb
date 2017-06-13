78640d6fca86a0c3a354ffd52fe16c9c74e32246
GregPirch <greg@iqcontrols.com>
2017-06-13 17:43:57 -0500

    Update WriteToHMITag.vb

--- WriteToHMITag.vb
+++ WriteToHMITag.vb
@@ -127,27 +127,3 @@ End Sub
 '------------------------------------------------------
 
 
-
-Private Sub Button_HoldBatch_Released()
-
-    Dim BatchSev As New BatchServer
-    Dim BatchResult As String
-    Dim BatchCreateID As String
-    Dim currentBatchNum As String
-    
- 
-    If BatchSev.GetItem("BatchListCt") <> "0" Then
-        BatchCreateID = BatchSev.GetItem("BLCreateID_1")
-        BatchResult = BatchSev.Execute("COMMAND(Item,ADV2\Admin," & BatchCreateID & ",HOLD)")
-        MsgBox ("PAUSED/HELD Batch Status: " & BatchResult & " (Create ID: " & BatchCreateID & ")")
-    Else
-        MsgBox ("Cannot PAUSE Batch. No Batches are Created OR Running!")
-    End If
-    
-    
-    
-End Sub
-
-Private Sub StringDisplay_BatchState_Change()
-
-End Sub
