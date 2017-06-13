89aec39ab9babfb51b32e014eea4e0a713fbbe07
GregPirch <greg@iqcontrols.com>
2017-06-13 17:36:52 -0500

    Create WriteToHMITag.vb

--- WriteToHMITag.vb
+++ WriteToHMITag.vb
@@ -83,42 +83,8 @@ Sub SetTagValue_Batch_Status()
     End If
 
 End Sub
+'------------------------------------------------------
 
-
-Private Sub Button_CreateBatch_Released()
-
-    Dim BatchSev As New BatchServer
-    Dim BatchResult As String
-    Dim currentBatchNum As String
-    
-    If BatchSev.GetItem("BatchListCt") = "0" Then
-    
-        BatchResult = BatchSev.Execute("BATCH(Item,ADV2\Admin,BULK_COMPLEX_BATCH.UOP,BATCH_ID,100,None,PARMS)")
-        MsgBox ("CREATE Batch Status and Batch Number: " & BatchResult)
-        currentBatchNum = CInt(Replace(Item1, "SUCCESS:", ""))
-    Else
-        MsgBox ("Cannot ADD Another Batch. A Batch Already Exist!")
-    End If
-    
-End Sub
-
-Private Sub Button_StartBatch_Released()
-
-    Dim BatchSev As New BatchServer
-    Dim BatchResult As String
-    Dim BatchCreateID As String
-    Dim currentBatchNum As String
-    
-    If BatchSev.GetItem("BatchListCt") <> "0" Then
-        BatchCreateID = BatchSev.GetItem("BLCreateID_1")
-        'MsgBox (BatchCreateID)
-        BatchResult = BatchSev.Execute("COMMAND(Item,ADV2\Admin," & BatchCreateID & ",START)")
-        MsgBox ("START Batch Status: " & BatchResult & " (Create ID: " & BatchCreateID & ")")
-    Else
-        MsgBox ("Cannot START Batch. No Batches are Created!")
-    End If
-    
-End Sub
 
 Private Sub Button_GetBatchState_Released()
 
