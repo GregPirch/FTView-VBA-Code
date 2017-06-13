24ee4d30ac1743d389dc7abc943a1cee7a96c8c4
GregPirch <greg@iqcontrols.com>
2017-06-13 17:38:45 -0500

    Create WriteToHMITag.vb

--- WriteToHMITag.vb
+++ WriteToHMITag.vb
@@ -21,7 +21,9 @@ Sub SetUpTagGroup()
     End If
 
 End Sub
+    
 '------------------------------------------------------
+    
 Sub SetTagValue_Second()
 
     On Error Resume Next
@@ -52,7 +54,9 @@ Sub SetTagValue_Second()
     End If
 
 End Sub
+    
 '------------------------------------------------------
+    
 Sub SetTagValue_Batch_Status()
 
     On Error Resume Next
@@ -84,7 +88,6 @@ Sub SetTagValue_Batch_Status()
 
 End Sub
 '------------------------------------------------------
-
 
 Private Sub Button_GetBatchState_Released()
 
@@ -119,45 +122,9 @@ Private Sub Button_GetBatchState_Released()
     Call SetTagValue_Batch_Status
     Call SetTagValue_Second
     
-    
-    
-
-
-
 End Sub
-
-Private Sub CarmelosTest_Released()
-
-    Dim BatchSev As New BatchServer
-    Dim BatchResult As String
-    Dim currentBatchNum As String
     
-    
-    BatchResult = BatchSev.GetItem("TimerSteps")
-    MsgBox BatchResult
-    Dim StringTemp() As String
-    StringTemp = Split(BatchResult, vbTab)
-    BatchResult = BatchSev.GetItem(StringTemp(0) & "TimerData")
-    MsgBox BatchResult
-    Dim TestString4 As String
-    TestString4 = BatchSev.GetItem(StringTemp(0) & "TimerStatus")
-    MsgBox TestString4
-    Dim TestSplit() As String
-    TestSplit = Split(TestString4, vbTab)
-    If TestString4 Like "*RUNNING*" Then
-    Dim StringTemp2() As String
-    StringTemp2 = Split(BatchResult, "REMAINING_TIME")
-    Dim StringTemp3() As String
-    StringTemp3 = Split(StringTemp2(1), vbTab)
-    MsgBox TestSplit(7)
-    Me.BatchTime.Caption = TestSplit(7)
-    Else
-    Me.BatchTime.Caption = ""
-    End If
-    
-    
-
-End Sub
+'------------------------------------------------------
 
 
 
